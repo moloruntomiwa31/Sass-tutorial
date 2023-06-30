538220f069f6c6a732bf5a464efc6b622ac95e63
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2023-06-30 13:35:15 +0100

    Added features

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector(".menu-btn")
 const hamburger = document.querySelector(".menu-btn .menu-btn___burger")
 const nav = document.querySelector(".nav")
 const menuNav = document.querySelector(".menu-nav")
+const navItems = document.querySelectorAll(".menu-nav___item")
 let showMenu = false
 
 menuBtn.addEventListener('click', () => {
@@ -9,11 +10,13 @@ menuBtn.addEventListener('click', () => {
         hamburger.classList.add("open")
         nav.classList.add("open")
         menuNav.classList.add("open")
+        navItems.forEach((item) => item.classList.add("open"))
         showMenu = true
     }else {
         hamburger.classList.remove("open")
         nav.classList.remove("open")
         menuNav.classList.remove("open")
+        navItems.forEach((item) => item.classList.remove("open"))
         showMenu = false
     }
 })
